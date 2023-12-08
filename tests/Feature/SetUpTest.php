5466b215efe102d625b16651f7254a53496b7847
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-09 00:31:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/SetUpTest.php
+++ tests/Feature/SetUpTest.php
@@ -1,13 +1,17 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\Attributes\WithMigration;
-
-use function Orchestra\Testbench\Pest\{afterApplicationCreated, setUp, usesTestingFeature};
+use function Orchestra\Testbench\Pest\{afterApplicationCreated, beforeApplicationDestroyed, setUp, usesTestingFeature};
 
 setUp(function ($setUp) {
     afterApplicationCreated(function () {
         config(['testbench.setUp' => true]);
+    });
+
+    beforeApplicationDestroyed(function () {
+        config(['testbench.tearDown' => true]);
     });
 
     usesTestingFeature(new WithMigration('laravel', 'queue'));
@@ -15,9 +19,14 @@ setUp(function ($setUp) {
     $setUp();
 });
 
-it('can resolve `afterApplicationCreated` via `setUp` helper', function () {
+it('can execute default setUpTheEnvironment via `setUp` helper', function () {
     expect($this->setUpHasRun)->toBe(true);
+    expect($this->app)->toBeInstanceOf(Application::class);
+});
+
+it('can resolve `afterApplicationCreated` via `setUp` helper', function () {
     expect(config('testbench.setUp'))->toBe(true);
+    expect(config('testbench.tearDown'))->toBe(null);
 });
 
 it('can resolve `usesTestingFeature` via `setUp` helper', function () {
