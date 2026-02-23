e5899b5905ffc2a64b3d97aa33c6e693e55ba4fa
Mior Muhammad Zaki <crynobone@gmail.com>
2026-02-23 21:15:10 +0800

    fix browser tests
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/BrowserTest.php
+++ tests/Browser/BrowserTest.php
@@ -2,8 +2,15 @@
 
 use Laravel\Dusk\Browser;
 
+use function Orchestra\Testbench\laravel_version_compare;
+
 it('can_browse_default_laravel_page')
     ->browse(function (Browser $browser) {
         $browser->visit('/')
-            ->assertSee('Documentation');
+            ->pause(500)
+            ->assertSee(
+                laravel_version_compare('12.0', '>=')
+                    ? 'Laravel has an incredibly rich ecosystem.'
+                    : 'Documentation'
+            );
     });
